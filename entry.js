99ec44b9ae17d9545563caed44b7e478777fe99f
Daniel Werthén <danielwerthen@gmail.com>
2015-02-11 21:38:14 +0100

    Live hot reloads

---
+++ entry.js
@@ -0,0 +1,16 @@
+require("./style.css");
+
+var React = require('react');
+var CommentBox = React.createClass({
+  render: function() {
+    return (
+      <div className="commentBox">
+        Hello, world! I am a CommentBox.
+      </div>
+    );
+  }
+});
+React.render(
+  <CommentBox />,
+  document.getElementsByTagName("body")[0]
+);

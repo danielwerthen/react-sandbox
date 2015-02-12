cb083619effb84951d6da397034aa298b04e8f59
Daniel Werthén <danielwerthen@gmail.com>
2015-02-12 22:06:11 +0100

    Stuff

--- entry.js
+++ entry.js
@@ -1,31 +1,33 @@
 require("./style.css");
 
 var React = require('react/addons');
-var Test = React.createClass({
+
+var App = React.createClass({
+  getInitialState: function () {
+    return { name: 'Daniel' };
+  },
+  onChange: function (e) {
+    var val = e.target.value;
+    this.setState({ name: val });
+  },
   render: function () {
-    return <div>test</div>;
-  }
-}); 
-var CommentBox = React.createClass({
-  render: function() {
-    return (
-      <div className="commentBox" style={{ width: 5 }}>
-        Hello, world! I am a CommentBox.
-        <form>
-          <input type="text" value="4" />
-          <Test />
-        </form>
-      </div>
-    );
+    var name = this.state.name,
+      nameTag = <span className="highlight">{name}</span>,
+      adj = "plain";
+
+    if (name.toLowerCase() === "daniel") {
+      adj = "awesome";
+    }
+    if (name.toLowerCase() === "ella") {
+      adj = "kick-ass";
+    }
+
+    return <div>
+      <h1>Hello {nameTag} how do you you?</h1>
+      <input type="text" value={this.state.name} onChange={this.onChange}/>
+      <p>{name.toUpperCase()} is a {adj} name</p>
+    </div>;
   }
 });
 
-module.exports = CommentBox;
-
-
-
-/*var TestUtils = React.addons.TestUtils,
-    _ = require('lodash');
-
-var rendered = TestUtils.renderIntoDocument(<CommentBox />);
-*/
+React.render(<App />, document.body);
